b4cfc874ee54e1fb250ebca1b28f0b06e6d1556f
Alexander MacKinnon <alexmackinnon55@gmail.com>
2021-01-24 10:48:56 -0500

    Track slider update

--- src/components/Player.js
+++ src/components/Player.js
@@ -81,18 +81,29 @@ const Player = ({
     // Check if the song is playing: adding this line of code twice since the 'return' may ignore it here
     playAudio(audioRef, isPlaying);
   };
-
+  // Add the styles
+  const trackAnim = {
+    transform: `translateX(${songInfo.animationPercentage}%)`,
+  };
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input
-          min={0}
-          max={songInfo.duration || 0}
-          value={songInfo.currentTime}
-          onChange={dragHandler}
-          type="range"
-        />
+        <div
+          style={{
+            background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]})`,
+          }}
+          className="track"
+        >
+          <input
+            min={0}
+            max={songInfo.duration || 0}
+            value={songInfo.currentTime}
+            onChange={dragHandler}
+            type="range"
+          />
+          <div style={trackAnim} className="animate-track"></div>
+        </div>
         <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
         {/* Fixes NaN timestamp glitch */}
       </div>
